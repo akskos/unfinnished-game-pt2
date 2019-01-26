29b2653ba04d5521668aea0493f8429edbc6191a
Akseli Koskinen <koskinen.akseli@gmail.com>
2019-01-26 11:22:58 +0200

    particles and player

--- src/MainScene.ts
+++ src/MainScene.ts
@@ -1,4 +1,4 @@
-import 'phaser';
+import Phaser from 'phaser';
 
 export default class MainScene extends Phaser.Scene {
     constructor() {
@@ -8,10 +8,21 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('computer', 'computer.jpg');
+        this.load.setBaseURL('assets/');
+        this.load.image('player', 'player.png');
+        this.load.image('particle', 'particle.png');
     }
 
     create() {
-        this.add.image(400, 400, 'computer');
+        const group = this.physics.add.staticGroup();
+        const player = group.create(400, 300, 'player').setScale(1).refreshBody();
+        // const computer = this.add.image(400, 300, 'computer');
+        const particles = this.add.particles('particle');
+        const emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: Phaser.BlendModes.ADD,
+        });
+        emitter.startFollow(player);
     }
 }
